1dd04daf2567253f9a06f998cae62bdc45bf6273
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-04-18 12:23:14 +0200

    adding setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ntt',
+    version='0.1',
+    description='pipeline module',
+    author='Romain Vuillemot',
+    author_email='romain.vuillemot@ec-lyon.fr',
+    packages=['ntt']
+)
