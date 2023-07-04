5301c2f6121515cc297906670f365a8f2fd4399d
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-07-04 21:47:33 +0200

    more files functions

--- src/ntt/utils/files.py
+++ src/ntt/utils/files.py
@@ -16,9 +16,19 @@ def delete_folder(path):
         os.rmdir(path)
 
 
+def create_folder(folder_path):
+    try:
+        os.mkdir(folder_path)
+        print(f"Folder created successfully: {folder_path}")
+    except FileExistsError:
+        print(f"Folder already exists: {folder_path}")
+    except Exception as e:
+        print(f"An error occurred while creating the folder: {e}")
+
+
 def delete_and_create_folder(path):
     delete_folder(path)
-    os.mkdir(path)
+    create_folder(path)
     return path
 
 
@@ -42,3 +52,12 @@ def remove_file_if_exists(file_path):
     else:
         print(f"File {file_path} does not exist, not removed.")
         return False
+
+
+def generate_shortcut_url(file_path="file.url", url="https://www.google.com"):
+    file_contents = "[{000214A0-0000-0000-C000-000000000046}]\n"
+    file_contents += "Prop3=19,2\n[InternetShortcut]\n"
+    file_contents += "URL={}\nIDList=\nHotKey=0\n".format(url)
+    with open(file_path, "w") as f:
+        f.write(file_contents)
+    return file_path
