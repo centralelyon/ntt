02b0213ac1c4155f8a8d4222022fefe86c702c4f
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-07-04 08:57:11 +0200

    commenting test as missing video @Sucio21

--- tests/test_sound_gap_measure.py
+++ tests/test_sound_gap_measure.py
@@ -1,6 +1,13 @@
 import os
-from ntt.sounds.Sound_generation import One_seconde_square_frequencies,Random_to_start,No_to_start,Vid2_decale,Dirac
+from ntt.sounds.Sound_generation import (
+    One_seconde_square_frequencies,
+    Random_to_start,
+    No_to_start,
+    Vid2_decale,
+    Dirac,
+)
 from ntt.sounds.Sound_gap_measure import sound_gap_measure
+
 
 def test_same_video():
     frequency1 = 440
@@ -9,25 +16,26 @@ def test_same_video():
     filename = "video"
     One_seconde_square_frequencies(percentage, frequency1, frequency2, filename)
 
-    path="samples/video.mp4"
-    res=sound_gap_measure(path,path)
+    path = "samples/video.mp4"
+    res = sound_gap_measure(path, path)
     os.remove(path)
     assert res == 0
 
-def test_decalage():
-    during=2
-    decal=1
-    name="video"
-    Vid2_decale(during,decal,name)
 
-    path1="samples/video.mp4"
-    path2="samples/videodecale.mp4"
-    res=sound_gap_measure(path2,path1)
+def test_decalage():
+    during = 2
+    decal = 1
+    name = "video"
+    Vid2_decale(during, decal, name)
+
+    path1 = "samples/video.mp4"
+    path2 = "samples/videodecale.mp4"
+    res = sound_gap_measure(path2, path1)
     os.remove(path1)
     os.remove(path2)
     assert res == decal
 
+
 if __name__ == "__main__":
     test_same_video()
-    test_decalage()
-
+    # test_decalage() # missing input file
