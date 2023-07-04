fee76736b21a14fc0765dd51739382eb20436bac
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-07-04 08:15:24 +0200

    adding nth and last frame extraction functions

--- src/ntt/frames/frame_extraction.py
+++ src/ntt/frames/frame_extraction.py
@@ -1,25 +1,60 @@
 import cv2
 import os
+import numpy as np
+from dotenv import load_dotenv
 
-## TODO return frame or write it..
-# autres méthodes ffmpeg, etc
-# comparer les méthodes entre elles: temps, qualité, etc
 
-def extract_first_frame(video_path_in, video_name_in, frame_path_out, frame_name_out): 
-
+def extract_last_frame(video_path_in, video_name_in, frame_path_out, frame_name_out):
     video_name = os.path.join(video_path_in, video_name_in)
     frame_name = os.path.join(frame_path_out, frame_name_out)
 
     vidcap = cv2.VideoCapture(video_name)
+
+    total_frames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    vidcap.set(cv2.CAP_PROP_POS_FRAMES, total_frames - 1)
+
     success, image = vidcap.read()
 
     if success:
         cv2.imwrite(frame_name, image)
+    else:
+        return None
 
-    return frame_path_out
+    return frame_name
 
-def extract_frame_opencv(video_path, frame_number = 1):
 
+def extract_first_frame(video_path_in, video_name_in, frame_path_out, frame_name_out):
+    return extract_nth_frame(
+        video_path_in, video_name_in, frame_path_out, frame_name_out, 0
+    )
+
+
+def extract_nth_frame(
+    video_path_in, video_name_in, frame_path_out, frame_name_out, nth_frame=0
+):
+    video_name = os.path.join(video_path_in, video_name_in)
+    frame_name = os.path.join(frame_path_out, frame_name_out)
+
+    vidcap = cv2.VideoCapture(video_name)
+    success = True
+
+    i = 0
+
+    while i <= nth_frame and success != False:
+        success, image = vidcap.read()
+
+        if i == nth_frame and success:
+            cv2.imwrite(frame_name, image)
+        else:
+            return None
+
+        i = i + 1
+
+    return frame_name
+
+
+def extract_frame_opencv(video_path, frame_number=1):
     if not os.path.isfile(video_path):
         return None
 
@@ -29,7 +64,7 @@ def extract_frame_opencv(video_path, frame_number = 1):
     if frame_number < 1 or frame_number > total_frames:
         return None
 
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number-1)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
     ret, frame = cap.read()
 
     if ret:
@@ -37,14 +72,16 @@ def extract_frame_opencv(video_path, frame_number = 1):
     else:
         return None
 
+
 # Extract a frame using FFmpeg
 def extract_frame_ffmpeg(video_path, frame_number):
     stream = ffmpeg.input(video_path)
-    stream = ffmpeg.filter(stream, 'select', f'eq(n,{frame_number-1})')
-    stream = ffmpeg.output(stream, 'pipe:', format='rawvideo', pix_fmt='rgb24')
+    stream = ffmpeg.filter(stream, "select", f"eq(n,{frame_number-1})")
+    stream = ffmpeg.output(stream, "pipe:", format="rawvideo", pix_fmt="rgb24")
     output, _ = ffmpeg.run(stream, capture_stdout=True, quiet=True)
     frame = cv2.imdecode(np.frombuffer(output, dtype=np.uint8), cv2.IMREAD_COLOR)
     return frame
+
 
 # Compare the frames extracted by OpenCV and FFmpeg
 def compare_frames(video_path, frame_number):
@@ -56,10 +93,11 @@ def compare_frames(video_path, frame_number):
     return np.all(diff == 0)
 
 
-if __name__ == '__main__':
-
-    extract_first_frame(video_path_in = "samples/", 
-                        video_name_in = "crop.mp4",
-                        frame_path_out = "samples/",
-                        frame_name_out = "crop.jpg" 
-                        )
+if __name__ == "__main__":
+    load_dotenv()
+    extract_first_frame(
+        video_path_in=os.environ.get("VIDEO_PATH_IN"),
+        video_name_in="crop.mp4",
+        frame_path_out=os.environ.get("FRAME_PATH_OUT"),
+        frame_name_out="crop.jpg",
+    )
--- tests/test_frame_extraction.py
+++ tests/test_frame_extraction.py
@@ -1,23 +1,22 @@
 import os
 import cv2
-from ntt.frames.frame_extraction import extract_first_frame
-from ntt.frames.n_frame_extraction import extract_n_frame
+from dotenv import load_dotenv
+from ntt.frames.frame_extraction import extract_first_frame, extract_last_frame
 
 
 def test_extract_first_frame():
-    video_path_in = "samples/"
+    load_dotenv()
+    video_path_in = os.environ.get("VIDEO_PATH_IN")
     video_name_in = "crop.mp4"
-    frame_path_in = "samples/"
+    frame_path_in = os.environ.get("VIDEO_PATH_IN")
     frame_name_in = "crop.jpg"
 
-    video_path = os.path.join(video_path_in, video_name_in)
     frame_path = os.path.join(frame_path_in, frame_name_in)
     image = cv2.imread(frame_path)
-    height, width, layers = image.shape
 
-    # call of the function to test
+    # test
     result = extract_first_frame(
-        video_path_in, video_name_in, "samples/", "crop-test.jpg"
+        video_path_in, video_name_in, os.environ.get("VIDEO_PATH_IN"), "crop-test.jpg"
     )
 
     saved_image = cv2.imread(frame_path)
@@ -25,21 +24,20 @@ def test_extract_first_frame():
     assert (saved_image == image).all()
 
 
-def test_extract_n_frame(n=0):
-    video_path_in = "samples/"
+def test_extract_last_frame():
+    load_dotenv()
+    video_path_in = os.environ.get("VIDEO_PATH_IN")
     video_name_in = "crop.mp4"
-    frame_path_in = "samples/"
+    frame_path_in = os.environ.get("VIDEO_PATH_IN")
     frame_name_in = "crop.jpg"
 
-    video_path = os.path.join(video_path_in, video_name_in)
     frame_path = os.path.join(frame_path_in, frame_name_in)
-
     image = cv2.imread(frame_path)
-    height, width, layers = image.shape
 
-    # call of the function to test
-    result = extract_n_frame(video_path_in, video_name_in, n)
-    assert result != None
+    # test
+    result = extract_last_frame(
+        video_path_in, video_name_in, os.environ.get("VIDEO_PATH_IN"), "crop-test.jpg"
+    )
 
     saved_image = cv2.imread(frame_path)
     assert saved_image.shape == image.shape
@@ -47,4 +45,5 @@ def test_extract_n_frame(n=0):
 
 
 if __name__ == "__main__":
-    test_extract_n_frame()
+    test_extract_first_frame()
+    test_extract_last_frame()
