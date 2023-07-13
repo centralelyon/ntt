ba2f140c408f43e172be7247d604d368ead8f2a7
Sucio <esteban.cosserat@gmail.com>
2023-07-13 16:30:25 +0200

    Update test_sound_gap_measure.py

--- tests/test_sound_gap_measure.py
+++ tests/test_sound_gap_measure.py
@@ -1,5 +1,5 @@
 import os
-from ntt.sounds.sound_generation import one_seconde_square_frequencies,vid2_decale
+from ntt.sounds.sound_generation import one_seconde_square_frequencies
 from ntt.sounds.sound_gap_measure import sound_gap_measure
 
 
@@ -17,11 +17,6 @@ def test_same_video():
 
 
 def test_decalage():
-    """during = 2
-    decal = 1
-    name = "video"
-    vid2_decale(during, decal, name)"""
-
     path1=f"{os.environ.get('VIDEO_PATH_IN')}2022_CF_Limoges_papillon_dames_50_finaleA_fixeDroiteCompressed_cut.mp4"
     path2=f"{os.environ.get('VIDEO_PATH_IN')}2022_CF_Limoges_papillon_dames_50_finaleA_fixeGaucheCompressed_cut.mp4"
     res = sound_gap_measure(path2, path1)
