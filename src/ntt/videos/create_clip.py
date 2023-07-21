0045c3317421d467b865419295aab4d02800be18
eradesaymeric <erades.aymeric@outlook.fr>
2023-07-21 10:41:39 +0200

    Ajout fonction de découpage de clip avec Opencv

--- src/ntt/videos/create_clip.py
+++ src/ntt/videos/create_clip.py
@@ -1,5 +1,6 @@
 import os
 from moviepy import editor
+import cv2 as cv
 
 
 def cut_video(
@@ -17,3 +18,50 @@ def cut_video(
     myclip_out.close()
 
     return video_file_out
+
+
+def cut_video_opencv(
+    video_file_in: str = os.path.join("samples", "crop.mp4"),
+    video_file_out: str = os.path.join("output", "crop_clip.mp4"),
+    start: int = 0,
+    end: int = 10
+) -> str:
+    """Cut video during a given time interval in frame"""
+
+    if start < 0:
+        print("Error: negativ start")
+        return(0)
+    elif start >= end:
+        print(start)
+        print(end)
+        print("Error: start >= end")
+        return(0)
+    
+    if not os.path.isfile(video_file_in):
+        print("Wrong video path")
+        return(0)
+    cap = cv.VideoCapture(video_file_in)
+    length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+    
+    ret, frame = cap.read()
+    fps = cap.get(cv.CAP_PROP_FPS)
+    if start > length:
+        print('Error: start > video length')
+        return(0)
+    if end > length:
+        print('Error: end > video length')
+        return(0)
+    
+    
+    cap.set(cv.CAP_PROP_POS_FRAMES, int(start))
+    
+    height, width, layers = frame.shape
+    frameSize = (width,height)
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    out = cv.VideoWriter(video_file_out, fourcc, fps, frameSize)
+    for i in range(end-start):
+        ret, frame = cap.read()
+        out.write(frame)
+    out.release()
+    
+    return(video_file_out)
