b94ee63b9f25cba3e79e027ab9c3a4825498c721
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-08-22 18:57:35 +0200

    using python_docs_theme for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ntt'
-copyright = '2023, Romain Vuillemot'
-author = 'Romain Vuillemot'
+project = "ntt"
+copyright = "2023, Romain Vuillemot"
+author = "Romain Vuillemot"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "python_docs_theme"
+html_static_path = ["_static"]
