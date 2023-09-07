f6a56d8d523b1145b1780639f1d3263685eb76bd
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-09-07 11:18:32 +0200

    function to blur a region

---
+++ src/ntt/frames/processing.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+def blur_frame(frame, kernel_size=(5, 5), region=None):
+    """
+    Apply Gaussian blur to a frame (NumPy array).
+
+    Parameters:
+        frame (numpy.ndarray): Input frame as a NumPy array.
+        kernel_size (tuple): Size of the Gaussian kernel for blurring.
+        region (tuple): Region of interest (x, y, width, height) to blur. If None, the entire frame will be blurred.
+
+    Returns:
+        numpy.ndarray: Blurred frame as a NumPy array.
+    """
+
+    if not isinstance(frame, np.ndarray):
+        raise ValueError("Input frame must be a NumPy array.")
+
+    blurred_frame = frame.copy()
+
+    if region:
+        x, y, width, height = region
+
+        if x < 0 or y < 0 or x + width > frame.shape[1] or y + height > frame.shape[0]:
+            raise ValueError("Specified region is out of frame bounds.")
+
+        region_of_interest = blurred_frame[y : y + height, x : x + width]
+        blurred_region = cv2.GaussianBlur(region_of_interest, kernel_size, 0)
+        blurred_frame[y : y + height, x : x + width] = blurred_region
+    else:
+        blurred_frame = cv2.GaussianBlur(blurred_frame, kernel_size, 0)
+
+    return blurred_frame
+
+
+input_frame = np.random.randint(
+    0, 256, size=(480, 640, 3), dtype=np.uint8
+)  # Example random frame
+blurred_frame = blur_frame(
+    input_frame, kernel_size=(15, 15), region=(100, 100, 200, 150)
+)
+
+
+# Display the original and blurred images side by side
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
+plt.imshow(input_frame)
+
+plt.subplot(1, 2, 2)
+plt.title("Blurred Image")
+plt.imshow(blurred_frame)
+
+plt.tight_layout()
+plt.show()
