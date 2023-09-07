eef97960db89bf790adc3028e46ed6e1bf2a57ee
fmiled <faroukmiled.prepa@gmail.com>
2023-09-07 12:18:55 +0200

    rectifying error in overlaying function

--- src/ntt/frames/overlay_two_images.py
+++ src/ntt/frames/overlay_two_images.py
@@ -2,14 +2,14 @@ import os, cv2
 
 
 def overlay_images(path_frames, name_frame1, name_frame2, name_output_frame):
-    path_frame1 = os.join(path_frames, name_frame1)
-    path_frame2 = os.join(path_frames, name_frame2)
+    path_frame1 = os.path.join(path_frames, name_frame1)
+    path_frame2 = os.path.join(path_frames, name_frame2)
 
     frame1 = cv2.imread(path_frame1)
     frame2 = cv2.imread(path_frame2)
 
     overlayed_frame = cv2.addWeighted(frame1, 0.5, frame2, 0.5, 0)
-    path_output_frame = os.join(path_frames, name_output_frame)
-    
+    path_output_frame = os.path.join(path_frames, name_output_frame)
+
     cv2.imwrite(path_output_frame, overlayed_frame)
     return overlayed_frame
