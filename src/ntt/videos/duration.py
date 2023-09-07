257adb83806244a6af7bd353f3abe9cf4f970141
Romain Vuillemot <romain.vuillemot@gmail.com>
2023-09-07 11:20:32 +0200

    removing duration using ffmeg cmd

--- src/ntt/videos/duration.py
+++ src/ntt/videos/duration.py
@@ -1,11 +1,38 @@
-import os
 import subprocess
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
 
 
-def remove_duration(input_file, output_file, duration):
+def remove_duration(input_file, output_file, start_time):
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i",
+        input_file,
+        "-y",
+        "-ss",
+        str(start_time),
+        "-vcodec",
+        "libx264",
+        "-g",
+        "10",
+        "-crf",
+        "24",
+        "-preset",
+        "superfast",
+        "-vf",
+        "format=yuv420p",
+        "-acodec",
+        "aac",
+        "-strict",
+        "experimental",
+        output_file,
+    ]
+
+    subprocess.run(ffmpeg_cmd)
+
+
+def remove_duration_movieclip(input_file, output_file, duration):
     video_clip = VideoFileClip(input_file)
 
     if video_clip.duration >= duration:
