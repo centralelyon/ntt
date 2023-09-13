40cab8f9bff21d6b3772094fa0163ce941792a18
fmiled <faroukmiled.prepa@gmail.com>
2023-09-13 11:19:52 +0200

    make opacities as parameter for overlay_images

--- src/ntt/frames/frame_overlay.py
+++ src/ntt/frames/frame_overlay.py
@@ -1,14 +1,15 @@
 import os, cv2
 
 
-def overlay_two_images(path_frames, name_frame1, name_frame2, path_output_frame):
+def overlay_two_images(path_frames, name_frame1, name_frame2, opacities, path_output_frame):
     path_frame1 = os.path.join(path_frames, name_frame1)
     path_frame2 = os.path.join(path_frames, name_frame2)
 
     frame1 = cv2.imread(path_frame1)
     frame2 = cv2.imread(path_frame2)
+    opacity_frame1,opacity_frame2=opacities[0],opacities[1]
 
-    overlayed_frame = cv2.addWeighted(frame1, 0.5, frame2, 0.5, 0)
+    overlayed_frame = cv2.addWeighted(frame1, opacity_frame1, frame2, opacity_frame2, 0)
 
     cv2.imwrite(path_output_frame, overlayed_frame)
     return overlayed_frame
