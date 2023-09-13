c3aab9bce780755807140e167159ee0cf41d1e9b
fmiled <faroukmiled.prepa@gmail.com>
2023-09-13 11:56:13 +0200

    create overlay_n_frames function

--- src/ntt/frames/frame_overlay.py
+++ src/ntt/frames/frame_overlay.py
@@ -1,7 +1,7 @@
 import os, cv2
 
 
-def overlay_two_images(path_frames, name_frame1, name_frame2, opacities, path_output_frame):
+def overlay_two_frames(path_frames, name_frame1, name_frame2, opacities, path_output_frame):
     path_frame1 = os.path.join(path_frames, name_frame1)
     path_frame2 = os.path.join(path_frames, name_frame2)
 
@@ -13,3 +13,19 @@ def overlay_two_images(path_frames, name_frame1, name_frame2, opacities, path_ou
 
     cv2.imwrite(path_output_frame, overlayed_frame)
     return overlayed_frame
+def overlay_n_frames(path_frames, frames,opacities,path_output_frame):
+    n=len(frames)
+    path_frame0=os.path.join(path_frames,frames[0])
+    path_frame1=os.path.join(path_frames,frames[1])
+    frame0=cv2.imread(path_frame0)
+    frame1=cv2.imread(path_frame1)
+    overlayed=cv2.addWeighted(frame0,opacities[0],frame1,opacities[1],0)
+    for i in range(2,n):
+        path_frame=os.path.join(path_frames,frames[i])
+        frame=cv2.imread(path_frame)
+        alpha=i+1
+        overlayed=cv2.addWeighted(overlayed,1-alpha,frame,alpha)
+    cv2.imwrite(path_output_frame,overlayed)
+    return(overlayed)
+
+
--- tests/test_overlay_images.py
+++ tests/test_overlay_images.py
@@ -1,8 +1,8 @@
-from ntt.frames.frame_overlay import overlay_two_images
+from ntt.frames.frame_overlay import overlay_two_images,overlay_n_frames
 import os,cv2
 from dotenv import load_dotenv
 load_dotenv()
-def test_overlay_images():
+def test_overlay_two_images():
     frames_path_in=f"{os.environ.get('PATH_IN')}"
     frame1_name="frame1.jpg"
     frame2_name="frame2.jpg"
@@ -14,6 +14,24 @@ def test_overlay_images():
     assert frame1.shape==frame2.shape
     opacities=[0.5]*2
     overlayed=overlay_two_images(frames_path_in,frame1_name,frame2_name,opacities,path_output_name)
+def test_overlay_n_images():
+    frames_path_in=f"{os.environ.get('PATH_IN')}"
+    frame1_name="frame1.jpg"
+    frame2_name="frame2.jpg"
+    frame3_name="frame3.jpg"
+    path_output_name=f"{os.environ.get('FRAME_PATH_OUT')}overlayed.jpg"
+    path_frame1=os.path.join(frames_path_in,frame1_name)
+    path_frame2=os.path.join(frames_path_in,frame2_name)
+    path_frame3=os.path.join(frames_path_in,frame3_name)
+    frame1=cv2.imread(path_frame1)
+    frame2=cv2.imread(path_frame2)
+    frame3=cv2.imread(path_frame3)
+    frames=[frame1,frame2,frame3]
+    assert frame1.shape==frame2.shape and frame1.shape==frame3.shape
+    opacities=[0.5]*3
+    overlayed=overlay_n_frames(frames_path_in,frames,opacities,path_output_name)
+
 
 if __name__=="__main__":
-    test_overlay_images()
+    test_overlay_two_images()
+    test_overlay_n_images()
