43802d3e540eced7594638d508e93d2ecd3bb21e
fmiled <faroukmiled.prepa@gmail.com>
2023-09-20 11:20:56 +0200

    create split_video function

---
+++ examples/ex_split_video.py
@@ -0,0 +1,10 @@
+from ntt.videos.split_video import split_video
+from dotenv import load_dotenv
+import os
+if __name__=="__main__":
+    load_dotenv()
+    video_path_in=f"{os.environ.get('VIDEO_PATH_IN')}"
+    video_name="2023_CF_Rennes_freestyle_hommes_50_finaleA_fixeDroite.mp4"
+    path_out=os.environ.get('PATH_OUT')
+    n=2
+    split_video(video_path_in,video_name,n,path_out)
