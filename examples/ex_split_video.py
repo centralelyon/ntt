33de5bcda74f059e0e8c8558abd7a9e8e91bf4d5
fmiled <faroukmiled.prepa@gmail.com>
2023-09-26 16:16:36 +0200

    format with Black

--- examples/ex_split_video.py
+++ examples/ex_split_video.py
@@ -1,10 +1,11 @@
 from ntt.videos.split_video import split_video_ffmpeg
 from dotenv import load_dotenv
 import os
-if __name__=="__main__":
+
+if __name__ == "__main__":
     load_dotenv()
-    video_path_in=f"{os.environ.get('VIDEO_PATH_IN')}"
-    video_name="2023_CF_Rennes_freestyle_hommes_50_finaleA_fixeDroite.mp4"
-    output_path=os.environ.get('PATH_OUT')
-    n=5
-    split_video_ffmpeg(video_path_in,video_name,output_path,n)
+    video_path_in = f"{os.environ.get('VIDEO_PATH_IN')}"
+    video_name = "2023_CF_Rennes_freestyle_hommes_50_finaleA_fixeDroite.mp4"
+    output_path = os.environ.get("PATH_OUT")
+    n = 5
+    split_video_ffmpeg(video_path_in, video_name, output_path, n)
