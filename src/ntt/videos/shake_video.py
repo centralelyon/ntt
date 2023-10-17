479db2a2332a71da409e33442a9691503866f540
fmiled <faroukmiled.prepa@gmail.com>
2023-10-17 12:22:51 +0200

    add destabilize function

--- src/ntt/videos/shake_video.py
+++ src/ntt/videos/shake_video.py
@@ -1,36 +1,32 @@
-import os,cv2
+import os, cv2
 import numpy as np
 from moviepy.editor import VideoClip
 from ntt.utils.random import random_translate_direction
-def shake_video_randomly(video_path_in,video_name,shake_intensity,video_path_out):
-    video=os.path.join(video_path_in,video_name)
+
+
+def shake_video_randomly(video_path_in, video_name, shake_intensity, video_path_out):
+    video = os.path.join(video_path_in, video_name)
     video = cv2.VideoCapture(video)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = video.get(cv2.CAP_PROP_FPS)
     video_writer = cv2.VideoWriter(video_path_out, fourcc, fps, (width, height))
     while True:
-        ret,frame=video.read()
+        ret, frame = video.read()
         if not ret:
             break
-        h,w,_=frame.shape
-        direction=random_translate_direction()
-        frame1=np.zeros_like(frame)
-        if direction =="up":
-            frame1[:h-shake_intensity]=frame[shake_intensity:h]
-        elif direction =="left":
-            frame1[:,:w-shake_intensity]=frame[:,shake_intensity:w]
-        elif direction =="down":
-            frame1[shake_intensity:h]=frame[:h-shake_intensity]
+        h, w, _ = frame.shape
+        direction = random_translate_direction()
+        frame1 = np.zeros_like(frame)
+        if direction == "up":
+            frame1[: h - shake_intensity] = frame[shake_intensity:h]
+        elif direction == "left":
+            frame1[:, : w - shake_intensity] = frame[:, shake_intensity:w]
+        elif direction == "down":
+            frame1[shake_intensity:h] = frame[: h - shake_intensity]
         else:
-            frame1[:,shake_intensity:w]=frame[:,:w-shake_intensity]
+            frame1[:, shake_intensity:w] = frame[:, : w - shake_intensity]
         video_writer.write(frame1)
     video_writer.release()
     video.release()
-        
-
-
-
-
-
