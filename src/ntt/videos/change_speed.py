4f909eedc82c4a6b61ebfd4ee244bbb319317a83
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-14 18:22:30 +0200

    Replace os.path functions with pathlib functions
    The description of the parameters should be completed and the function
    tested.

--- src/ntt/videos/change_speed.py
+++ src/ntt/videos/change_speed.py
@@ -1,16 +1,30 @@
 import os
+from pathlib import Path
 from moviepy.editor import VideoFileClip
-from dotenv import load_dotenv
-
-load_dotenv()
+import dotenv
 
 
-def change_video_speed(
-    video_file_in: str = os.path.join(os.environ.get("VIDEO_PATH_IN"), "ping.mp4"),
-    video_file_out: str = os.path.join(os.environ.get("PATH_OUT"), "ping_speed.mp4"),
-    speed_factor: int = 1,
-) -> str:
-    """change the video speed with a factor > 1 accelerates, <1 slows down"""
+def change_video_speed(video_file_in: str, video_file_out: str, speed_factor: int = 1) -> str:
+    """Change the video speed with a factor > 1 accelerates, <1 slows down.
+    TODO : Describe parameters
+
+    Args:
+        video_file_in (str): _description_
+        video_file_out (str): _description_
+        speed_factor (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        str: _description_
+    """
+    # TODO : Loading the environment variables should be done in the calling
+    # script, not in the ntt library
+    env_vars = dotenv.dotenv_values()
+
+    if video_file_in is None:
+        video_file_in = Path(env_vars.get('VIDEO_PATH_IN')) / "ping.mp4"
+
+    if video_file_out is None:
+        video_file_out = Path(env_vars.get('PATH_OUT')) / "ping_speed.mp4"
 
     video_in = VideoFileClip(video_file_in)
     video_out = video_in.fx(VideoFileClip.speedx, speed_factor)
