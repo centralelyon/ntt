7fdad8316a4d8831f4bf7941c79acd263b1b886a
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-19 14:42:22 +0200

    Fixed linter warnings - unused  import
    There's no code in this example script.

--- examples/ex_synchro_soundtracks.py
+++ examples/ex_synchro_soundtracks.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0114
 
-from pydub import AudioSegment
-from pydub.utils import mediainfo
+# from pydub import AudioSegment
+# from pydub.utils import mediainfo
 
 if __name__ == "__main__":
     # TODO : do something
