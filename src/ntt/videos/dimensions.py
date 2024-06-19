70869a93d53f69e94212e4b65a5c5946ff27b9d2
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-19 14:28:27 +0200

    Fixed minor linter warning - whitespaces

--- src/ntt/videos/dimensions.py
+++ src/ntt/videos/dimensions.py
@@ -18,7 +18,7 @@ def get_video_dimensions(video_path):
         width, height = clip.size
         clip.close()
         return width, height
-    
+
     except Exception as e:
         print(f"Error: {e}")
         return None, None
