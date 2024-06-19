1f4b13825c4778bd016dac08cb0cf414b3ba2954
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-19 10:32:10 +0200

    Fixed linter issues on draw primitives

--- src/ntt/draw/polygone.py
+++ src/ntt/draw/polygone.py
@@ -1,23 +1,33 @@
+"""TODO : polygone module provides ...
+"""
+
+from typing import Tuple
+
 import cv2
 import numpy as np
-from typing import Tuple
 
 
 def draw_polygones(
     frame: np.ndarray,
-    points: Tuple[Tuple] = [[0, 0], [0, 1], [1, 1], [1, 0]],
+    points: Tuple[Tuple] = None,
     couleur: Tuple[int, int, int] = (0, 0, 255),
     epaisseur: int = 2,
 ) -> None:
-    """Ajoute a la frame un polygone du degrès de la taille de points, à l'epaisseur et couleur choisit.
-    Les points seront relier dans leurs ordre dans le paramètre points.
+    """Ajoute a la frame un polygone du degrès de la taille de points, à
+    l'epaisseur et couleur choisies. Les points seront relier dans leurs
+    ordre dans le paramètre points.
 
     Args:
         frame (np.ndarray): _description_
-        points (Tuple[Tuple], optional): _description_. Defaults to [[0,0],[0,1],[1,1],[1,0]].
-        couleur (Tuple[int, int, int], optional): _description_. Defaults to (0, 0, 255).
-        epaisseur(int) : epaisseur du tracé
+        points (Tuple[Tuple], optional): _description_.
+            Defaults to [[0, 0], [0, 1], [1, 1], [1, 0]].
+        couleur (Tuple[int, int, int], optional): _description_.
+            Defaults to (0, 0, 255).
+        epaisseur (int, optional): Épaisseur du tracé. Defaults to 2.
     """
+    # https://pylint.readthedocs.io/en/latest/user_guide/messages/warning/dangerous-default-value.html
+    if points is None:
+        points = [[0, 0], [0, 1], [1, 1], [1, 0]]
 
     n_lines = len(points)
     for i in range(n_lines):
--- src/ntt/draw/primitives.py
+++ src/ntt/draw/primitives.py
@@ -1,29 +1,55 @@
+"""TODO : primitives module provides ...
+"""
+
+from typing import Tuple
+
 import cv2
 import numpy as np
-from typing import Tuple
 
 
 def write_text(
     frame: np.ndarray,
     text: str = "Text",
-    posXY: Tuple[int, int] = (10, 50),
+    pos_x_y: Tuple[int, int] = (10, 50),
     color: Tuple[int, int, int] = (0, 0, 255),
     thickness: int = 2,
 ) -> None:
-    """write some text at a given position"""
+    """Write some text at a given position.
+
+    Args:
+        frame (np.ndarray): _description_
+        text (str, optional): _description_. Defaults to "Text".
+        pos_x_y (Tuple[int, int], optional): _description_. Defaults to (10, 50).
+        color (Tuple[int, int, int], optional): _description_. Defaults to (0, 0, 255).
+        thickness (int, optional): _description_. Defaults to 2.
+
+    Returns:
+        _type_: _description_
+    """
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, text, posXY, font, 1, color, thickness, cv2.LINE_4)
+    cv2.putText(frame, text, pos_x_y, font, 1, color, thickness, cv2.LINE_4)
     return frame
 
 
 def draw_rectangle(
     frame: np.ndarray,
-    posXY: Tuple[int, int] = (10, 10, 30, 30),
+    pos_x_y: Tuple[int, int] = (10, 10, 30, 30),
     color: Tuple[int, int, int] = (0, 0, 255),
 ) -> None:
-    """draw a rectangle given 2 points"""
+    """Draw a rectangle given 2 points.
+
+    Args:
+        frame (np.ndarray): _description_
+        pos_x_y (Tuple[int, int], optional): _description_.
+            Defaults to (10, 10, 30, 30).
+        color (Tuple[int, int, int], optional): _description_.
+            Defaults to (0, 0, 255).
+
+    Returns:
+        _type_: _description_
+    """
     thickness = 1
-    cv2.rectangle(frame, posXY, color, thickness)
+    cv2.rectangle(frame, pos_x_y, color, thickness)
     return frame
 
 
@@ -33,14 +59,30 @@ def draw_line(
     end_point: Tuple[int, int] = (100, 100),
     color: Tuple[int, int, int] = (0, 0, 255),
 ) -> None:
-    """draw a line given 2 points"""
+    """Draw a line given 2 points.
+
+    Args:
+        frame (np.ndarray): _description_
+        start_point (Tuple[int, int], optional): _description_. Defaults to (10, 10).
+        end_point (Tuple[int, int], optional): _description_. Defaults to (100, 100).
+        color (Tuple[int, int, int], optional): _description_. Defaults to (0, 0, 255).
+    """
     cv2.line(frame, start_point, end_point, color, thickness=2)
 
 
 def draw_crosshair(
     frame: np.ndarray, x: int, y: int, size=20, thickness=2, color=(0, 255, 0)
 ):
-    """draw a crosshair on the image"""
+    """Draw a crosshair on the image.
+
+    Args:
+        frame (np.ndarray): _description_
+        x (int): _description_
+        y (int): _description_
+        size (int, optional): _description_. Defaults to 20.
+        thickness (int, optional): _description_. Defaults to 2.
+        color (tuple, optional): _description_. Defaults to (0, 255, 0).
+    """
     cv2.line(frame, (x, y - size), (x, y + size), color, thickness)
     cv2.line(frame, (x - size, y), (x + size, y), color, thickness)
 
@@ -48,7 +90,17 @@ def draw_crosshair(
 def draw_bright_circle(
     image: np.ndarray, center: Tuple, radius: int, brightness_factor=50
 ):
-    """enhance a given circular region in the image with a brightness factor"""
+    """Enhance a given circular region in the image with a brightness factor.
+
+    Args:
+        image (np.ndarray): _description_
+        center (Tuple): _description_
+        radius (int): _description_
+        brightness_factor (int, optional): _description_. Defaults to 50.
+
+    Returns:
+        _type_: _description_
+    """
 
     mask = np.zeros_like(image)  # mask
     cv2.circle(mask, center, radius, (255, 255, 255), -1)  # draw a circle on the mask
@@ -65,6 +117,16 @@ def draw_bright_circle(
 
 
 def draw_grid(frame, rows: int, cols: int):
+    """_summary_
+
+    Args:
+        frame (_type_): _description_
+        rows (int): _description_
+        cols (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
     frame_size = (frame.shape[1], frame.shape[0])
 
     for row in range(rows + 1):
