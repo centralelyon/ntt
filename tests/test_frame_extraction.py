81baeae965f4f749b07513afb256ea361bb2e685
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-19 14:15:52 +0200

    test_frame_extraction was writing in the input path
    Some frames that seems to be the results of test or examples were
    commited (crop-test.jpg, crop-ex.jpg) and were modified by each test
    execution.
    
    Seems that the input video for frame extraction (crop.mp4) has only one
    frame. Thus extract_first = extract_last and extract_nth does not work.

--- tests/test_frame_extraction.py
+++ tests/test_frame_extraction.py
@@ -13,20 +13,21 @@ from ntt.frames.frame_extraction import (
 
 
 def test_extract_first_frame():
-    """_summary_
-    """
+    """_summary_"""
     load_dotenv()
     video_path_in = os.environ.get("VIDEO_PATH_IN")
     video_name_in = "crop.mp4"
     frame_path_in = os.environ.get("VIDEO_PATH_IN")
     frame_name_in = "crop.jpg"
+    frame_path_out = os.environ.get("FRAME_PATH_OUT")
+    frame_name_out = "crop-test-first.jpg"
 
     frame_path = os.path.join(frame_path_in, frame_name_in)
     image = cv2.imread(frame_path)
 
     # test
     result = extract_first_frame(
-        video_path_in, video_name_in, os.environ.get("VIDEO_PATH_IN"), "crop-test.jpg"
+        video_path_in, video_name_in, frame_path_out, frame_name_out
     )
 
     saved_image = cv2.imread(frame_path)
@@ -36,20 +37,21 @@ def test_extract_first_frame():
 
 
 def test_extract_last_frame():
-    """_summary_
-    """
+    """_summary_"""
     load_dotenv()
     video_path_in = os.environ.get("VIDEO_PATH_IN")
     video_name_in = "crop.mp4"
     frame_path_in = os.environ.get("VIDEO_PATH_IN")
     frame_name_in = "crop.jpg"
+    frame_path_out = os.environ.get("FRAME_PATH_OUT")
+    frame_name_out = "crop-test-last.jpg"
 
     frame_path = os.path.join(frame_path_in, frame_name_in)
     image = cv2.imread(frame_path)
 
     # TODO: result not used
     result = extract_last_frame(
-        video_path_in, video_name_in, os.environ.get("VIDEO_PATH_IN"), "crop-test.jpg"
+        video_path_in, video_name_in, frame_path_out, frame_name_out
     )
 
     saved_image = cv2.imread(frame_path)
@@ -58,13 +60,12 @@ def test_extract_last_frame():
 
 
 def test_extract_nth_frame():
-    """_summary_
-    """
+    """_summary_"""
     load_dotenv()
     video_path_in = os.environ.get("VIDEO_PATH_IN")
     video_name_in = "crop.mp4"
     frame_path_out = os.environ.get("FRAME_PATH_OUT")
-    frame_name_out = "nth_frame.jpg"
+    frame_name_out = "crop-test-nth-frame.jpg"
 
     # get total number of frames
     cap = cv2.VideoCapture(os.path.join(video_path_in, video_name_in))
