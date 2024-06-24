2fb0a5a23cff30e2fe63fc0492169658402a41c1
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-24 15:41:42 +0200

    Use pytest fixtures and pathlib for file path

--- tests/test_draw_piscine.py
+++ tests/test_draw_piscine.py
@@ -1,54 +1,58 @@
-"""TODO : test_draw_piscine ...
+"""TODO : test_draw_swimming_pool ...
 """
 
 import json
-import os
 
 import cv2
-from dotenv import load_dotenv
 from ntt.draw.polygone import draw_polygones
 
-load_dotenv()
 
-
-def extract_piscine(jsonfile):
-    """_summary_
+def extract_polygone(json_file_path):
+    """Extracting polygone coordinates.
 
     Args:
-        jsonfile (_type_): _description_
+        json_file_path (Path or str): json file path
 
     Returns:
-        _type_: _description_
+        list of lists: Polygone coordinates
     """
     try:
-        with open(jsonfile, "rb") as f:
+        with open(json_file_path, "rb") as f:
             data = json.load(f)
+
         x0, y0 = map(int, data["videos"][1]["srcPts"][0])
         x1, y1 = map(int, data["videos"][1]["srcPts"][1])
         x2, y2 = map(int, data["videos"][1]["srcPts"][2])
         x3, y3 = map(int, data["videos"][1]["srcPts"][3])
+
         return [[y0, x0], [y1, x1], [y2, x2], [y3, x3]]
 
     except Exception as e:
-        print("Execption", e)
+        print(f"Exception when trying to open {json_file_path} : {e}")
 
 
-def test_draw_piscine():
-    """draw swimming pool with ntt"""
-    jsonfile = os.path.join(
-        os.environ.get("PATH_IN"), "2023_CF_Rennes_freestyle_hommes_50_finaleA.json"
-    )
-    video_name = os.path.join(
-        os.environ.get("VIDEO_PATH_IN"),
-        "2023_CF_Rennes_freestyle_hommes_50_finaleA_fixeDroite.mp4",
-    )
+def test_draw_swimming_pool(sample_path_in, sample_path_out):
+    """Draw swimming pool with ntt.
+
+    Args:
+        sample_path_in (Path): input path
+        sample_path_out (Path): output path
+    """
+
+    jsonfile = sample_path_in / "2023_CF_Rennes_freestyle_hommes_50_finaleA.json"
+    video_path_in = (
+        sample_path_in / "2023_CF_Rennes_freestyle_hommes_50_finaleA_fixeDroite.mp4"
+    )  # noqa - E501 # pylint: disable=C0301
+
     # open video
-    piscine = extract_piscine(jsonfile)
+    piscine = extract_polygone(jsonfile)
     try:
-        video = cv2.VideoCapture(video_name)
+        video = cv2.VideoCapture(video_path_in)
+
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = video.get(cv2.CAP_PROP_FPS)
+        
         for coords in piscine:
             assert (
                 coords[0] >= 0
@@ -58,16 +62,17 @@ def test_draw_piscine():
             )
 
     except Exception as e:
-        print("Exception", e)
+        print(
+            "Exception when trying to extract swimming pool from"
+            "{video_path_in} : {e}",
+            e,
+        )
 
     # Define the codec for the output video
     fourcc = cv2.VideoWriter_fourcc("M", "J", "P", "G")
-    output_video = cv2.VideoWriter(
-        os.path.join(os.environ.get("PATH_OUT"), "output_piscine.mp4"),
-        fourcc,
-        fps,
-        (width, height),
-    )
+    video_path_out = sample_path_out / "output_piscine.mp4"
+    output_video = cv2.VideoWriter(video_path_out, fourcc, fps, (width, height))
+
     while True:
         # read frame from video
         ret, frame = video.read()
@@ -81,11 +86,7 @@ def test_draw_piscine():
             break
 
         # Wait for the 'q' key to quit
+
     # liberate resources
     video.release()
     output_video.release()
-
-
-if __name__ == "__main__":
-    # TODO : Remove this block
-    test_draw_piscine()
