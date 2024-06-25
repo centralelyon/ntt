f08bb7f1eb9f1f06a6846d7c0b4dde863bf45dc5
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-06-25 14:44:35 +0200

    Use pytest fixtures for input and output path

--- tests/test_split_video.py
+++ tests/test_split_video.py
@@ -1,29 +1,21 @@
 """TODO : test_split_video ...
 """
 
-import os
-
 import pytest
-from dotenv import load_dotenv
 from ntt.videos.split_video import split_video_ffmpeg
 
-load_dotenv()
 
+def test_split_video_negative_n(sample_path_in, sample_path_out):
+    """Test ntt split_video_ffmpeg function.
 
-def test_split_video_negative_n():
-    """_summary_
+    Args:
+        sample_path_in (Path): input path
+        sample_path_out (Path): output path
     """
-    video_path_in = os.environ.get("NTT_SAMPLES_PATH_IN")
     video_name = "swimming_start_small.mp4"
-    output_path = os.environ.get("PATH_OUT")
     n = -5  # Test with a negative value for n
 
     with pytest.raises(ValueError) as e:
-        split_video_ffmpeg(video_path_in, video_name, output_path, n)
+        split_video_ffmpeg(sample_path_in, video_name, sample_path_out, n)
 
     assert str(e.value) == "Number of segments (n) must be greater than zero."
-
-
-if __name__ == "__main__":
-    # TODO : Remove this block
-    test_split_video_negative_n()
