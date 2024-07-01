55ec6163846ad1a9308b5c429c1d4188f1ce39ca
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-07-01 10:02:06 +0200

    Linter warning on whitespaces

--- src/ntt/frames/frame_overlay.py
+++ src/ntt/frames/frame_overlay.py
@@ -65,14 +65,14 @@ def overlay_n_frames(path_frames, frames, opacities, path_output_frame):
     path_frame1 = input_path / frames[1]
     frame0 = cv2.imread(path_frame0)
     frame1 = cv2.imread(path_frame1)
-    
+
     overlayed = cv2.addWeighted(frame0, opacities[0], frame1, opacities[1], 0)
-    
+
     for i in range(2, n):
         path_frame = input_path / frames[i]
         frame = cv2.imread(path_frame)
         overlayed = cv2.addWeighted(overlayed, 1 - opacities[i], frame, opacities[i], 0)
-    
+
     cv2.imwrite(output_path, overlayed)
-    
+
     return overlayed
