ac0418a429e800e713472362d1100c874f9a93bb
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-07-01 16:12:49 +0200

    Removing path and file manipulation from the core functions
    Loading the environment variables should be done in the calling script,
    not in the ntt library

--- src/ntt/videos/change_speed.py
+++ src/ntt/videos/change_speed.py
@@ -1,9 +1,6 @@
 """TODO : change_speed module provides ...
 """
 
-from pathlib import Path
-
-import dotenv
 from moviepy.editor import VideoFileClip
 
 
@@ -21,16 +18,6 @@ def change_video_speed(
     Returns:
         str: _description_
     """
-    # TODO : Loading the environment variables should be done in the calling
-    # script, not in the ntt library
-    env_vars = dotenv.dotenv_values()
-
-    if video_file_in is None:
-        video_file_in = Path(env_vars.get("VIDEO_PATH_IN")) / "ping.mp4"
-
-    if video_file_out is None:
-        video_file_out = Path(env_vars.get("PATH_OUT")) / "ping_speed.mp4"
-
     video_in = VideoFileClip(video_file_in)
     video_out = video_in.fx(VideoFileClip.speedx, speed_factor)
     video_out.write_videofile(video_file_out)
