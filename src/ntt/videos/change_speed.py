840e365d19d4b45d54b703c127873013401f0333
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-07-02 11:00:05 +0200

    Moved str conversion to change_video_speed
    Some moviepy functions use ffmpeg as a library which takes file path as
    string not Path. Conversion to str before calling VideoFileClip or
    write_videofile is needed.

--- src/ntt/videos/change_speed.py
+++ src/ntt/videos/change_speed.py
@@ -1,26 +1,32 @@
 """TODO : change_speed module provides ...
 """
 
+from __future__ import annotations
+
+import os
+
 from moviepy.editor import VideoFileClip
 
 
 def change_video_speed(
-    video_file_in: str, video_file_out: str, speed_factor: int = 1
+    video_file_in: str | os.PathLike,
+    video_file_out: str | os.PathLike,
+    speed_factor: int = 1,
 ) -> str:
     """Change the video speed with a factor > 1 accelerates, <1 slows down.
     TODO : Describe parameters
 
     Args:
-        video_file_in (str): _description_
-        video_file_out (str): _description_
+        video_file_in (str or Path): Full path to the input video file
+        video_file_out (str or Path): Full Path to the output video file
         speed_factor (int, optional): _description_. Defaults to 1.
 
     Returns:
         str: _description_
     """
-    video_in = VideoFileClip(video_file_in)
+    video_in = VideoFileClip(str(video_file_in))
     video_out = video_in.fx(VideoFileClip.speedx, speed_factor)
-    video_out.write_videofile(video_file_out)
+    video_out.write_videofile(str(video_file_out))
     video_in.close()
     video_out.close()
 
--- tests/test_change_video_speed.py
+++ tests/test_change_video_speed.py
@@ -12,13 +12,14 @@ def test_change_video_speed(sample_path_in, sample_path_out):
         sample_path_in (Path): input path
         sample_path_out (Path): output path
     """
-    video_file_in = str(sample_path_in / "ping.mp4")
-    video_file_out = str(sample_path_out / "ping_speed.mp4")
+    video_file_in = sample_path_in / "ping.mp4"
+    video_file_out = sample_path_out / "ping_speed.mp4"
 
     change_video_speed(video_file_in, video_file_out, 1)
 
-    video_in = VideoFileClip(video_file_in)
-    video_out = VideoFileClip(video_file_out)
+    # Conversion to str need for ffmpeg underlying functions
+    video_in = VideoFileClip(str(video_file_in))
+    video_out = VideoFileClip(str(video_file_out))
 
     # info video_in.reader.nframes does not give the sama values of nb frames
     nb_frames_video_in = video_in.fps * video_in.duration
@@ -28,8 +29,9 @@ def test_change_video_speed(sample_path_in, sample_path_out):
     assert nb_frames_video_in == nb_frames_video_out
 
     change_video_speed(video_file_in, video_file_out, 2)
-    video_in = VideoFileClip(video_file_in)
-    video_out = VideoFileClip(video_file_out)
+    
+    video_in = VideoFileClip(str(video_file_in))
+    video_out = VideoFileClip(str(video_file_out))
 
     nb_frames_video_in = video_in.fps * video_in.duration
     nb_frames_video_out = video_out.fps * video_out.duration
@@ -38,8 +40,9 @@ def test_change_video_speed(sample_path_in, sample_path_out):
     assert nb_frames_video_in > nb_frames_video_out
 
     change_video_speed(video_file_in, video_file_out, 0.5)
-    video_in = VideoFileClip(video_file_in)
-    video_out = VideoFileClip(video_file_out)
+
+    video_in = VideoFileClip(str(video_file_in))
+    video_out = VideoFileClip(str(video_file_out))
 
     nb_frames_video_in = video_in.fps * video_in.duration
     nb_frames_video_out = video_out.fps * video_out.duration
