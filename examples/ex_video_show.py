58adba0f4be27d184e6881ecb104743461339dbc
Françoise Conil <francoise.conil@insa-lyon.fr>
2024-07-03 16:41:49 +0200

    Removing path and file manipulation from the core function
    Removing path and file manipulation from the core function.
    
    The graphic interface examples could be placed in the examples folder
    only.

--- examples/ex_video_show.py
+++ examples/ex_video_show.py
@@ -1,11 +1,19 @@
 # pylint: disable=C0114
 
 import os
+from pathlib import Path
 
-from dotenv import load_dotenv
+import dotenv
 from ntt.videos.show import show_video
 
+# https://peps.python.org/pep-0008/#constants
+VIDEO_NAME_IN = "ping.mp4"
+
 if __name__ == "__main__":
-    load_dotenv()
-    input_video_path = os.path.join(os.environ.get("PATH_IN"), "ping.mp4")
-    show_video(input_video_path)
+    ev_path_parent = Path(dotenv.find_dotenv()).parent
+
+    dotenv.load_dotenv()
+
+    path_in = Path(ev_path_parent / os.environ.get("PATH_IN"))
+
+    show_video(path_in / VIDEO_NAME_IN)
--- src/ntt/videos/show.py
+++ src/ntt/videos/show.py
@@ -6,18 +6,23 @@ import cv2
 
 def show_video(video_path, window_name="Video"):
     """Show a video in a window.
+    TODO : A library should not display graphic interface.
 
     Args:
-        video_path (_type_): _description_
+        video_path (str or Path): Full path to the output video
         window_name (str, optional): _description_. Defaults to "Video".
     """
     cap_output = cv2.VideoCapture(video_path)
-    while cap_output.isOpened():
-        ret, frame_output = cap_output.read()
-        if not ret:
-            break
-        cv2.imshow(window_name, frame_output)
+
+    got_frame = True
+    while cap_output.isOpened() and got_frame:
+        got_frame, frame_output = cap_output.read()
+
+        if got_frame:
+            cv2.imshow(window_name, frame_output)
+
         if cv2.waitKey(30) & 0xFF == 27:
             break
+
     cap_output.release()
     cv2.destroyAllWindows()
